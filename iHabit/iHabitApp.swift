d992281ce70c0de37600ad4ec0c9b33dfd7dee8d
Hitodama0 <biagior00@gmail.com>
2023-02-08 14:54:28 +0100

    Initial Commit

---
+++ iHabit/iHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHabitApp.swift
+//  iHabit
+//
+//  Created by Biagio Ricci on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct iHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
